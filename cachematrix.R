9640b9b08b9ce9e736f18da23a3c5b23deec5ea6
Deb Wells <dwells@txfb-ins.com>
2015-03-17 19:22:35 -0500

    Updating Comments

--- cachematrix.R
+++ cachematrix.R
@@ -1,19 +1,32 @@
 ## These functions used in combination help convserve resources by
 ## caching the inverse of a matrix for re-use instead of recomputation
+## First pass your square numeric matrix into makeCacheMatrix, store it in a variable
+## Pass the variable into cacheSolve to receive the inverse of the matrix.
 
 ## makeCacheMatrix will set and get the value of matrix from another 
 ## environment so that the value will remain in memory for the next
 ## call of the function
 
-makeCacheMatrix <- function(x = matrix()) {
+makeCacheMatrix <- function(x =numeric()) {
+    ## Clear the initial value
     m <- NULL
-    set <- function(y) {
-        x <<- y
+    set <- function(NewVar) {
+        x <<- NewVar
+        ## New Matrix entered, so flush the cache
         m <<- NULL
     }
-    get <- function() x
-    setsolve <- function(solve) m <<- solve
-    getsolve <- function() m
+    ## get the matrix back
+    get <- function() {
+        x
+    }
+    setsolve <- function(solve) {
+        ## Tell it what you want to do to the Matrix    
+        m <<- solve
+    }
+    ## Get the result and return the list
+    getsolve <- function(){
+        m
+    }
     list(set = set, get = get,
          setsolve = setsolve,
          getsolve = getsolve)
@@ -26,7 +39,8 @@ makeCacheMatrix <- function(x = matrix()) {
 ## save the matrix inverse into cache for the next call.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    ## Return a matrix that is the inverse of 'x'
+    ## Get the cached Value
     m <- x$getsolve()
     if(!is.null(m)) {
         ## if the matrix has already been solved, return the cached value
